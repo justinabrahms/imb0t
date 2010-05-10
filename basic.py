c6fc72c375fc3832d99c7e3dc361292c9186d73a
Justin Lilly <justin@justinlilly.com>
2010-05-10 18:37:49 -0400

    Reordering things a bit.

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
 def handle_jira(bot, user, channel, msg, groups):
     for match in groups:
-        bot.say(channel, "http://jira.invitemedia.com/browse/%s" % match)
+        ticket_url = "http://jira.invitemedia.com/browse/%s" % match
+        bot.say(channel, ticket_url)
+        
