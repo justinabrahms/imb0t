96b83fb8e8d1cf362e8ea5c81f2b38eb14e89ce8
Justin Lilly <justin@justinlilly.com>
2010-12-15 01:26:22 -0500

    Removing python version.

--- python/basic.py
+++
@@ -1,5 +0,0 @@
-def handle_jira(bot, user, channel, msg, groups):
-    for match in groups:
-        ticket_url = "http://jira.invitemedia.com/browse/%s" % match
-        bot.say(channel, ticket_url)
-        
