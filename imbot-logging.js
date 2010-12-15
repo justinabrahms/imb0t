7a78e1d01f300e408d38460571d8e70eeeaadcf9
Justin Lilly <justin@justinlilly.com>
2010-12-15 01:30:15 -0500

    Fixing callbacks for logging module.

--- imbot-logging.js
+++ imbot-logging.js
@@ -3,11 +3,14 @@ var fs = require('fs');
 var plugin = {
     'name': 'logging'
     , 'callbacks': [
-        [/(.*)/, function() {
+        {
+            re: /(.*)/, 
+            fn: function(client, from, to, msg, matches) {
              fs.open(this.plugins[this.name].log_dir, 'a', function(err, fd) {
                  fs.write(fd, from + " => " + to + ": " + msg + "\r\n");
              });
-         }]
+         }
+        }
     ]
     , 'state': {
         log_dir: '/tmp/imb0t.log'
